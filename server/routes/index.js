ae4e27a3eb941fe6286b99d073c50e182ebab19d
18263231066 <18263231066@163.com>
2018-05-21 13:49:12 +0800

    getborrowbookurl

--- server/routes/index.js
+++ server/routes/index.js
@@ -43,6 +43,9 @@ router.get('/userBooksNum', controllers.userBooksNum)
 // GET 获取图书评论
 router.get('/getComments', controllers.getComments)
 
+// GET 获取新书上架
+router.get('/getBorrowBookUrl', controllers.getBorrowBookUrl)
+
 // GET 获取图书详情
 router.get('/getBookDetails', controllers.getBookDetails)
 
