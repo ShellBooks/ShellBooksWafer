08c4419b54e8b6fbab7f51c16bf637d17a69b168
18263231066 <18263231066@163.com>
2018-05-28 22:04:12 +0800

    just

--- server/controllers/getLikeBook.js
+++ server/controllers/getLikeBook.js
@@ -3,10 +3,10 @@ const { mysql } = require('../qcloud')
 module.exports = async ctx => {
   let uid = ctx.query.uid ? ctx.query.uid : -1
 
-  let likelist = await mysql.raw('SELECT like.bid, bname, author, image FROM like, book WHERE like.uid = ? AND like.bid = book.bid', [uid])
+  let likelist = await mysql.raw('SELECT `like`.bid, bname, author, image FROM `like`, book WHERE `like`.bid = book.bid AND `like`.uid = ?', [uid])
 
   ctx.state.data = {
-    likelist: likelist[0] //为什么取一项？？0是有效 1是无效？
+    likelist: likelist
   }
 
 }
