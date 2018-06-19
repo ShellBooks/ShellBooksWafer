4915374a018ae5ccd326f77330035b273ecadd2b
yvettemuki <373628977@qq.com>
2018-06-19 18:09:50 +0800

    界面调整
    活动管理页面界面调整

--- client/pages/uploadAct/uploadAct.js
+++ client/pages/uploadAct/uploadAct.js
@@ -2,7 +2,7 @@
 var config = require('../../config')
 var util = require('../../utils/util.js')
 
-var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+var sliderWidth = 30; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
 
@@ -188,7 +188,9 @@ Page({
       },
       success: res => {
         console.log(res)
-
+        wx.redirectTo({
+          url: 'uploadAct',
+        })
       }
     })
   }
